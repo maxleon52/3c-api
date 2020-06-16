60c5b1c34ee1959449e6ca5489b7f9f25b64d6d1
maxleon52 <maxleon522@gmail.com>
2020-06-16 14:34:44 -0300

    Validation with yup

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Yup = require("yup");
 
 const User = require("../models/User");
 const authConfig = require("../config/auth");
@@ -7,6 +8,19 @@ const authConfig = require("../config/auth");
 module.exports = {
   async create(req, res) {
     try {
+      // Validação de preenchimento de campos
+      const schema = Yup.object().shape({
+        email: Yup.string().email().required(),
+        password: Yup.string().required(),
+      });
+
+      if (!(await schema.isValid(req.body))) {
+        return res
+          .status(400)
+          .json({ message: "Erro de validação de campos." });
+      }
+
+      // Tentando criar sessão
       const { email, password } = req.body;
 
       // Verificando se usuário(e-mail) existe
@@ -33,7 +47,9 @@ module.exports = {
         }),
       });
     } catch (error) {
-      return res.status(400).json({ message: error });
+      return res
+        .status(400)
+        .json({ message: "Ocorreu um erro inesperado, contate o suporte." });
     }
   },
 };
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,24 @@
-const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const Yup = require("yup");
+const User = require("../models/User");
 const { update } = require("../models/User");
 
 module.exports = {
   async create(req, res) {
     try {
+      // Validação de preenchimento de campos
+      const schema = Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().email().required(),
+        password: Yup.string().required().min(6),
+      });
+
+      if (!(await schema.isValid(req.body))) {
+        return res
+          .status(400)
+          .json({ message: "Erro de validação de campos." });
+      }
+
       // Verifica se existe um EMAIL cadastrado
       // PASSWORD é o input do front, mas na TB é password_has, ele meio que simboliza um campo VIRTUAL
       const { name, email, password } = req.body;
@@ -30,38 +44,66 @@ module.exports = {
   },
 
   async update(req, res) {
-    console.log(req.userId);
-    const { name, email, oldPassword, password } = req.body;
+    try {
+      // Validação de preenchimento de campos
+      const schema = Yup.object().shape({
+        name: Yup.string(),
+        email: Yup.string().email(),
+        oldPassword: Yup.string().min(6),
+        password: Yup.string()
+          .min(6)
+          .when("oldPassword", (oldPassword, field) =>
+            oldPassword ? field.required() : field
+          ),
+        confirmPassword: Yup.string().when("password", (password, field) =>
+          password ? field.required().oneOf([Yup.ref("password")]) : field
+        ),
+      });
 
-    const user = await User.findById(req.userId);
-    console.log(user);
-
-    if (email && email !== user.email) {
-      const userExists = await User.findOne({ email });
-
-      if (userExists) {
-        return res.status(401).json({
-          message: "Esse e-mail ja está sendo usado, use outro e-mail válido.",
-        });
+      if (!(await schema.isValid(req.body))) {
+        return res
+          .status(400)
+          .json({ message: "Erro de validação de campos." });
       }
-    }
 
-    if (
-      oldPassword &&
-      !(await bcrypt.compare(oldPassword, user.password_hash))
-    ) {
-      return res.status(401).json({ message: "Senha antiga inválida." });
-    }
+      // Tentando atualizar dados
+      const { name, email, oldPassword, password } = req.body;
+      const user = await User.findById(req.userId);
 
-    const password_hash = await bcrypt.hash(password, 8);
+      // Vericando se Email foi preenchido e buscando no BD
+      if (email && email !== user.email) {
+        const userExists = await User.findOne({ email });
 
-    const response = await User.findByIdAndUpdate(
-      user._id,
-      { name, email, password_hash },
-      {
-        new: true,
+        if (userExists) {
+          return res.status(401).json({
+            message:
+              "Esse e-mail ja está sendo usado, use outro e-mail válido.",
+          });
+        }
       }
-    );
-    return res.json(response);
+
+      // verificando se senha antiga foi preenchida e se bate
+      if (
+        oldPassword &&
+        !(await bcrypt.compare(oldPassword, user.password_hash))
+      ) {
+        return res.status(401).json({ message: "Senha antiga inválida." });
+      }
+
+      // Criptografando nova senha
+      const password_hash = await bcrypt.hash(password, 8);
+      const response = await User.findByIdAndUpdate(
+        user._id,
+        { name, email, password_hash },
+        {
+          new: true,
+        }
+      );
+      return res.json(response);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ message: "Ocorreu um erro inesperado, contate o suporte." });
+    }
   },
 };
